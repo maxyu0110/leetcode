50ae30a28e75c3b6d92af593bceb521df9ddcaad
maxyu0110 <maxyu0110@users.noreply.github.com>
2016-09-06 22:15:46 -0700

    [339. Nested List Weight Sum][Accepted]committed by LeetCode Extension

---
+++ 339-Nested-List-Weight-Sum/solution.java
@@ -0,0 +1,35 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+public class Solution {
+    public int depthSum(List<NestedInteger> nestedList) {
+        return helper(nestedList, 1);
+    }
+    private int helper(List<NestedInteger> nestedList, int level) {
+        int res = 0;
+        if(nestedList == null) return res;
+        for(NestedInteger cur: nestedList){
+            if(cur.isInteger()){
+                res += cur.getInteger() * level;
+            }
+            else {
+                res += helper(cur.getList(), level + 1);
+            }
+        }
+        return res;
+    }
+}
